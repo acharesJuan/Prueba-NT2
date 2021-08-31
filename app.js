d6fd106ae039243462891e531db717849ec9c6b7
juani <acharesjuan@gmail.com>
2021-08-30 22:46:17 -0300

    .

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const listaDeProductos = [{nombre:"hamburguesa",precio:450},{nombre:"hamburguesa",precio:450},{nombre:"hamburguesa",precio:450}]
-
+const listaDeProductos = [{nombre:"hamburguesa",precio:450}
+,{nombre:"papafrita",precio:50}
+,{nombre:"coca",precio:20}
+,{nombre:"pancho",precio:200}
+,{nombre:"otra hamburguesa",precio:150}
+,{nombre:"hamburguesa",precio:450}]
 
 function filtrarProductos(valor){
     const productosFiltrados = listaDeProductos.filter(element => element.precio < valor)
-
-    productosFiltrados.forEach(producto => {
+    document.getElementById("lista").innerHTML = ""
+    productosFiltrados.forEach(producto => {      
         const elemento = document.createElement('p')
+        //CREA UN <p></p>
         elemento.innerText = producto.nombre
         document.getElementById("lista").appendChild(elemento)
-
     })
 }
